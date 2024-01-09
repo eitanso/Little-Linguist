7bd52766dbcf3ff8fbbe1ea560b6f5e6b066b54c
TalRozenberg <tal198666@gmail.com>
2024-01-09 20:25:52 +0200

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,14 +4,15 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { CategoriesComponent } from './categories/categories.component';
 import { MatTableModule } from '@angular/material/table';
+import { FooterComponent } from "./footer/footer.component";
 
 export class YourModuleName { }
 @Component({
-  selector: 'app-root',
-  standalone: true,
-imports: [CommonModule, RouterOutlet,HeaderComponent,MatTableModule],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [CommonModule, RouterOutlet, HeaderComponent, MatTableModule, FooterComponent]
 })
 export class AppComponent {
   title = 'Little Linguist';
