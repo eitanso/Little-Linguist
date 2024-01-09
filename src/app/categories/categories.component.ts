a0a9ad703a352af04166c32a576860aebfaf4853
TalRozenberg <tal198666@gmail.com>
2024-01-09 19:31:50 +0200

    .

--- src/app/categories/categories.component.ts
+++ src/app/categories/categories.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-categories',
@@ -6,15 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./categories.component.css']
 })
 export class CategoriesComponent {
+  categories: MatTableDataSource<any> | undefined;
 addCategory() {
 throw new Error('Method not implemented.');
 }
+/*
   categories = [
     { name: 'קטגוריה 1', words: ['מילה 1', 'מילה 2', 'מילה 3'], lastModified: 'תאריך 1' },
     { name: 'קטגוריה 2', words: ['מילה 4', 'מילה 5', 'מילה 6'], lastModified: 'תאריך 2' },
     { name: 'קטגוריה 3', words: ['מילה 7', 'מילה 8', 'מילה 9'], lastModified: 'תאריך 3' }
   ];
-
+*/
   editCategory(category: any) {
     
   }
