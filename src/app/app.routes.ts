8af705ab1954c2d88e2dd6cb3d0f6ab10a9c4ab7
TalRozenberg <tal198666@gmail.com>
2024-01-10 18:43:43 +0200

    .

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { CategoriesComponent } from './categories/categories.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+    path: '',
+    component: CategoriesComponent,
+}];
+
--- src/app/categories/categories.component.ts
+++ src/app/categories/categories.component.ts
@@ -1,23 +1,26 @@
 import { Component } from '@angular/core';
-import { MatTableDataSource } from '@angular/material/table';
+import { MatTableModule } from '@angular/material/table';
 
 @Component({
+ imports: [MatTableModule],
+ standalone: true, 
   selector: 'app-categories',
   templateUrl: './categories.component.html',
   styleUrls: ['./categories.component.css']
 })
 export class CategoriesComponent {
-  categories: MatTableDataSource<any> | undefined;
 addCategory() {
 throw new Error('Method not implemented.');
 }
-/*
+
   categories = [
     { name: 'קטגוריה 1', words: ['מילה 1', 'מילה 2', 'מילה 3'], lastModified: 'תאריך 1' },
     { name: 'קטגוריה 2', words: ['מילה 4', 'מילה 5', 'מילה 6'], lastModified: 'תאריך 2' },
     { name: 'קטגוריה 3', words: ['מילה 7', 'מילה 8', 'מילה 9'], lastModified: 'תאריך 3' }
   ];
-*/
+  displayedColumns: string[] = ['name','words','lastModified','actions'];
+
+
   editCategory(category: any) {
     
   }
