e8725304b4be9df8a3f6b41cdd5d837296bc6f2c
eitanso <eitan@solomovich.co.il>
2024-01-28 21:42:50 +0200

    fdsfdfd

---
+++ src/app/new-category/new-category.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-category',
+  standalone: true,
+  imports: [],
+  templateUrl: './new-category.component.html',
+  styleUrl: './new-category.component.css'
+})
+export class NewCategoryComponent {
+
+}
