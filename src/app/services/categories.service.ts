c8bca33b4e34205fda5042b3162e281a0f62597c
eitanso <eitan@solomovich.co.il>
2024-02-03 16:02:20 +0200

    .

--- src/app/services/categories.service.ts
+++ src/app/services/categories.service.ts
@@ -1,53 +1,38 @@
+import { Category } from "../shared/model/wordsCategory";
 import { Injectable } from '@angular/core';
-import { Category } from '../../shared/model/categories';
-
 
 @Injectable({
   providedIn: 'root'
 })
 
-export class EditCategoriesService {
-  category: any;
-  update(lastModifiedDate:Category): void {
-    if (this.category.has(Category.lastModifiedDate)) {
-      this.category.set(Category.lastModifiedDate, lastModifiedDate);
-    }
-  }
-} 
 
-export class DeleteCategoriesService{
-  Category: any;
-  delete(id: number): void {
-   this.Category.delete(id);
- }
+export class categoryService {
+    categories = new Map<number, Category>();
+    nextId= 1;
+
+    constructor() { }
+    list() : Category[] {
+        return Array.from(this.categories.values());
+      }
+
+      get(id : number) : Category | undefined {
+        console.log('Fetching category with ID:', id);
+        return this.categories.get(id);
+      }
+    
+      add(newCategoryData:Category) {
+        newCategoryData.id = this.nextId
+        this.categories.set(this.nextId, newCategoryData);
+        this.nextId++;
+      }
+    
+      update(existingCategory : Category) : void {
+        if (this.categories.has(existingCategory.id)) {
+          this.categories.set(existingCategory.id, existingCategory);
+        }
+      }
+
+      delete(existingCategoryId : number) : void {
+        this.categories.delete(existingCategoryId);
+      }
 }
-
-export class AddCategoriesService{
-  add(NewCategory : Category) {
-    let newId = this.nextId;
-    Category.id = newId;
-    this.categories.set(newId,this.category);
-    ++ this.nextId;
-   }
-  category(newId: number, category: any) {
-    throw new Error('Method not implemented.');
-  }
-  categories = new Map<number, Category>();
-  nextId= 0;
-
-}
-
-
-/*
- 
-  list(): Category[] {
-    return Array.from(this.categories.values())
- 
-  }
-  get(id: number): Category | undefined {
-    return this.categories.get(id);
-   }
-   
- 
-
-*/
