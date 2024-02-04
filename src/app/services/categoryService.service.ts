e679e67b5481432a417098e6e1b84dad9486bf8f
eitanso <eitan@solomovich.co.il>
2024-02-04 17:36:59 +0200

    .

--- src/app/services/categoryService.service.ts
+++ src/app/services/categoryService.service.ts
@@ -1,4 +1,4 @@
-import { WordCategory } from "../../shared/model/wordCategory";
+import { Category } from "../../shared/model/wordCategory";
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -7,26 +7,26 @@ import { Injectable } from '@angular/core';
 
 
 export class categoryService {
-    categories = new Map<number, WordCategory>();
+    categories = new Map<number, Category>();
     nextId= 1;
 
     constructor() { }
-    list() : WordCategory[] {
+    list() : Category[] {
         return Array.from(this.categories.values());
       }
 
-      get(id : number) : WordCategory | undefined {
+      get(id : number) : Category | undefined {
         console.log('Fetching category with ID:', id);
         return this.categories.get(id);
       }
     
-      add(newCategoryData:WordCategory) {
+      add(newCategoryData:Category) {
         newCategoryData.id = this.nextId
         this.categories.set(this.nextId, newCategoryData);
         this.nextId++;
       }
     
-      update(existingCategory : WordCategory) : void {
+      update(existingCategory : Category) : void {
         if (this.categories.has(existingCategory.id)) {
           this.categories.set(existingCategory.id, existingCategory);
         }
