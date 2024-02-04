f409b34c7e6fdba28dc2d03d166439fe88846ba6
eitanso <eitan@solomovich.co.il>
2024-02-04 18:24:59 +0200

    .

--- src/app/categories/categories.component.ts
+++ src/app/categories/categories.component.ts
@@ -2,13 +2,10 @@ import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTableModule } from '@angular/material/table';
-<<<<<<< HEAD
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 
-=======
 import { Router } from '@angular/router';
->>>>>>> 5453b6c9f5fc033c4fc66c2cba2c8bc694fdfa0d
 
 @Component({
  imports: [MatTableModule, MatButtonModule,MatIconModule],
@@ -24,9 +21,7 @@ throw new Error('Method not implemented.');
 }
 
   categories = [
-    { name: 'חיות', words: 3, lastModified: '01/01/2024' },
-    { name: 'מספרים', words: 3, lastModified: '01/01/2024' },
-    { name: 'צבעים', words: 3, lastModified: '01/01/2024' }
+    
   ];
   displayedColumns: string[] = ['name','words','lastModified','actions'];
 
@@ -45,4 +40,4 @@ throw new Error('Method not implemented.');
     this.router.navigate(['/new-category']);
   
 
-}
+}}
