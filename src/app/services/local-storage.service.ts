f99afb8f18b61b86c39f9787d53c22197298b417
TalRozenberg <tal198666@gmail.com>
2024-02-17 14:01:56 +0200

    .

--- src/app/services/local-storage.service.ts
+++ src/app/services/local-storage.service.ts
@@ -1,9 +1,40 @@
 import { Injectable } from '@angular/core';
+import { Category } from '../../shared/model/wordCategory';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LocalStorageService {
+  categories= new Map<number, Category>();
+  nextId= 1;
 
   constructor() { }
-}
+
+  list() : Category[] {
+    return Array.from(this.categories.values());
+   }
+
+   get(id : number) : Category | undefined {
+    return this.categories.get(id);
+   }
+
+   add(newCategoryData:Category) {
+    newCategoryData.id = this.nextId
+    this.categories.set(this.nextId, newCategoryData);
+    this.nextId++;
+    }
+    
+    update(existingCategories: Category) : void{
+      if(this.categories.has(existingCategories.id)) {
+        this.categories.set(existingCategories.id, existingCategories);
+      }
+    }
+
+    delete(existingCategoriesId : number) : void {
+      if (this.categories.has(existingCategoriesId)){
+        this.categories.delete(existingCategoriesId);
+      }
+    }
+   }
+
+
